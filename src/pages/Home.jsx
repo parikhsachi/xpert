cdf30594cb65c22923538025fb5c9a727fe78100
ianfs4 <ianfslater4@gmail.com>
2025-05-02 14:01:33 -0500

    Added test message for button press

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 const Home = () => {
+  const handleSendPrompt = async () => {
+    try {
+      // send api request to backend
+      console.log("Button clicked!");
+
+      // switch router to response page? not sure about this yet, maybe it will output the text on the same UI
+    } catch(err) {
+      console.error("Error processing user prompt:", err);
+    }
+  }
   return (
     <>
       <h1 className="text-4xl font-bold text-nu-purple mb-4">Welcome to Xpert.ai</h1>
@@ -12,7 +22,10 @@ const Home = () => {
         placeholder="Enter your question"
         className="w-full p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-nu-purple"
       />
-      <button className="mt-4 bg-nu-purple hover:bg-nu-purple-hover text-white font-semibold py-2 px-6 rounded">
+      <button
+        onClick={handleSendPrompt}
+        className="mt-4 bg-nu-purple hover:bg-nu-purple-hover text-white font-semibold py-2 px-6 rounded"
+      >
         Get Answer
       </button>
       <p className="mt-4 text-muted text-sm">Papers, Blogs, Interviews</p>
